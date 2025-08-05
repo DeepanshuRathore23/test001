a065d09eef24db3235e624f0bd0eb9427ada3d1b
DeepanshuRathore23 <deepanshurathore2004@gmail.com>
2025-08-05 20:54:50 +0530

    testing commit

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -24,29 +24,41 @@ export default function Events() {
     getEvents();
   }, []);
 
-  return (
-    <div className="min-h-screen mt-[90vh] bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-7xl mx-auto">
-        
-
-        <div className="text-center mb-12">
-          <h1 className="text-4xl font-bold text-[#d59f0f] mb-4">School Events</h1>
-          <p className="text-lg text-gray-600">Stay updated with our latest events and activities</p>
-        </div>
-
-        <div className="flex justify-end mb-6">
-          <Link href='/updateEvents'>
-            <button className="p-3 bg-[#d59f0f] rounded-xl text-white">Manage Events</button>
-          </Link>
+  if(events.length) {
+    return (
+      <div className="min-h-screen mt-[90vh] bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto">
           
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {events.map((event, index) => (
-            <EventCard key={index} {...event} />
-          ))}
+  
+          <div className="text-center mb-12">
+            <h1 className="text-4xl font-bold text-[#d59f0f] mb-4">School Events</h1>
+            <p className="text-lg text-gray-600">Stay updated with our latest events and activities</p>
+          </div>
+  
+          <div className="flex justify-end mb-6">
+            <Link href='/updateEvents'>
+              <button className="p-3 bg-[#d59f0f] rounded-xl text-white">Manage Events</button>
+            </Link>
+            
+          </div>
+  
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {events.map((event, index) => (
+              <EventCard key={index} {...event} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div className="min-h-screen mt-[90vh] bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+          No events available
+        </div>
+      </>
+    );
+  }
+
+  
 }
